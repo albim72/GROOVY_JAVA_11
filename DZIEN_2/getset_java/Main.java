ba74d98b950169ffa817de3cefd3f6683929fe11
albim72 <marcin.albiniak72@gmail.com>
2025-11-13 13:25:40 +0100

    Update Main.java

--- DZIEN_2/getset_java/Main.java
+++ DZIEN_2/getset_java/Main.java
@@ -4,5 +4,9 @@ public class Main {
         p.setAge(25);
         System.out.println("Imię: " + p.getName() + ",wiek: " + p.getAge());
 //        System.out.println(p.age); -> nie ma dosepu do age bo jest private
+
+        Person pd = new Person("Anna",56);
+        pd.setName("Joanna");
+        System.out.println("Imię: " + pd.getName() + ",wiek: " + pd.getAge());
     }
 }
