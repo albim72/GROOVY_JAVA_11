e475cfcc6bab93d5ec2cab24712b4aebbb1495fc
albim72 <marcin.albiniak72@gmail.com>
2025-11-13 14:06:18 +0100

    Create Order.java

---
+++ DZIEN_2/groovy_java/Order.java
@@ -0,0 +1,17 @@
+public class Order {
+    private final double amount;
+    private final String customer;
+    
+    public Order(double amount, String customer) {
+        this.amount = amount;
+        this.customer = customer;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+}
