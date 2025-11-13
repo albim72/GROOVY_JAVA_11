fefc01c659da9d47cb54f5f4d03eeea137817d69
albim72 <marcin.albiniak72@gmail.com>
2025-11-13 14:30:04 +0100

    Update Main.groovy

--- DZIEN_2/groovy_java/Main.groovy
+++ DZIEN_2/groovy_java/Main.groovy
@@ -1,9 +1,15 @@
-class Main{
+class Main {
     static void main(String[] args) {
-        Order order = new Order(1560.00,"Marcin")
+        // WŁĄCZ rozszerzenia przed użyciem Order
+        OrderExtensions.enable()
+
+        Order order = new Order(1560.00, "Marcin")
         println("Customer: ${order.customer}, amount: ${order.amount}")
 
-        double totalWithVat = order.amount *1.23
+        double totalWithVat = order.amount * 1.23
         println("Cena z VAT: ${totalWithVat}")
+
+        println(order.summary())
+        println("With VAT(23%): ${order.withVat(0.23)}")
     }
 }
