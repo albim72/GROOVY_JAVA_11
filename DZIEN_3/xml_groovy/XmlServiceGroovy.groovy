48a0c19c0e5bbdd4f1552adcec3632f02767fb4a
albim72 <marcin.albiniak72@gmail.com>
2025-11-14 11:53:27 +0100

    Create XmlServiceGroovy.groovy

---
+++ DZIEN_3/xml_groovy/XmlServiceGroovy.groovy
@@ -0,0 +1,34 @@
+import groovy.xml.MarkupBuilder
+import groovy.xml.XmlSlurper
+
+
+class XmlServiceGroovy {
+    List<Person> readPeople(File file) {
+        def xml = new XmlSlurper().parse(file)
+        //xml.people.person -> lista węzłów <person>
+        xml.person.collect {
+            node ->
+                new Person(
+                        name: node.name.text(),
+                        age: node.age.text().toInteger(),
+                        city: node.city.text()
+                )
+        }
+    }
+
+    void writePeople(File file,List<Person> people) {
+        def writer = new StringWriter()
+        def xml = new MarkupBuilder(writer)
+        xml.people {
+            people.each {p->
+                person {
+                    name(p.name)
+                    age(p.age)
+                    city(p.city)
+                }
+            }
+        }
+        file.text = writer.toString()
+    }
+
+}
